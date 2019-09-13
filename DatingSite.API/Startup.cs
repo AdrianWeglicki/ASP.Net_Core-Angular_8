58a8aacf15a9891e10542af9d03f1599cf01ec0d
Adrian Węglicki <adrian.weglicki@gmail.com>
2019-09-13 15:15:46 +0200

    Display data from API in SPA

--- DatingSite.API/Startup.cs
+++ DatingSite.API/Startup.cs
@@ -29,6 +29,7 @@ public class Startup
         {
             services.AddDbContext<DataContext>(x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -43,7 +44,7 @@ public class Startup
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-
+            app.UseCors(x=> x.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
             app.UseHttpsRedirection();
             app.UseMvc();
         }
