0d07760b48f7e9f1db8060f69c8caac110b197a4
Adrian Węglicki <adrian.weglicki@gmail.com>
2019-09-22 22:43:16 +0200

    Add auth service, add components: home, nav, register

---
+++ DatingSite.SPA/src/app/nav/nav.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../_services/auth.service';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.css']
+})
+export class NavComponent implements OnInit {
+
+  model: any = {};
+
+
+  constructor(private authService: AuthService) { }
+
+  ngOnInit() {
+  }
+
+  login() {
+    this.authService.login(this.model).subscribe(next => {
+      console.log('You are sign up!');
+    }, error => {
+      console.log('Error sign up');
+    });
+  }
+
+  loggedIn() {
+    const token = localStorage.getItem('token');
+    return !!token;
+  }
+
+  logout() {
+    localStorage.removeItem('token');
+    console.log('You are logout');
+  }
+  
+}
